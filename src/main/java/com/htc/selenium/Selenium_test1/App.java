c8b527c6d2e56154aabb188089aa4c6741a6c6a8
narasmu <muralibanuchandra@gmail.com>
2021-10-11 15:26:42 +0530

    environment setup created

---
+++ src/main/java/com/htc/selenium/Selenium_test1/App.java
@@ -0,0 +1,13 @@
+package com.htc.selenium.Selenium_test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
